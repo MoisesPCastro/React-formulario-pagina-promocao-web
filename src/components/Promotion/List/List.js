6c3139a62d8c22809df85024ebb33f79e7b335ba
MouchesPires <82000885+MoisesPiresCastro@users.noreply.github.com>
2021-08-15 20:56:39 -0300

    inicio

---
+++ src/components/Promotion/List/List.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PromotionCard from '../Card/Card';
+import './List.css';
+
+const PromotionList = ({ loading, error, promotions }) => {
+  console.log({ loading, error, promotions });
+  if (error) {
+    return <div>"Ops", algo deu errado! </div>;
+  }
+  if (loading || promotions === null) {
+    return <div>Carregando...</div>;
+  }
+
+  if (promotions.length === 0) {
+    return <div>Nenhum resultado encontrado</div>;
+  }
+
+  return (
+    <div className="promotion-list">
+      {promotions.map((promotion) => (
+        <PromotionCard promotion={promotion} />
+      ))}
+    </div>
+  );
+}
+
+export default PromotionList;
